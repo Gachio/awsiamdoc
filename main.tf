0380b97e18e2684cec120206f5bdfef0fec70dc1
Arm Bun <bellotaophyc@outlook.com>
2022-07-20 08:28:01 +0000

    initial commit to the armpolicy branch

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+data "aws_iam_policy_document" "example" {
+  statement {
+    actions   = ["s3:ListAllMyBuckets"]
+    resources = ["arn:aws:s3:::*"]
+    effect = "Allow"
+  }
+  statement {
+    actions   = ["s3:*"]
+    resources = [aws_s3_bucket.bucket.arn]
+    effect = "Allow"
+  }
+}
+
+
 terraform {
   required_providers {
     aws = {
@@ -38,7 +52,9 @@ resource "aws_s3_bucket_acl" "bucket" {
 resource "aws_iam_policy" "policy" {
   name        = "${random_pet.pet_name.id}-policy"
   description = "My test policy"
+  policy = data.aws_iam_policy_document.example.json
 
+/*
   policy = <<EOT
 {
   "Version": "2012-10-17",
@@ -61,5 +77,16 @@ resource "aws_iam_policy" "policy" {
   ]
 }
 EOT
+*/
+
+}
+
+resource "aws_iam_user_policy_attachment" "attachment" {
+  user       = aws_iam_user.new_user.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+output "rendered_policy" {
+  value = data.aws_iam_policy_document.example.json
 }
 
